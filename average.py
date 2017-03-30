94c30a9a3984e58964e741e09b03dc22186b9857
Ian Larson <ilarson2020@pingry.org>
2017-03-30 12:43:18 -0400

    i have added an average, so now it averages the number of numbers there is.

--- average.py
+++ average.py
@@ -4,8 +4,11 @@ sum = 0
 
 average = 0
 
-readfile = f.readlines
+readfile = f.readlines()
+f.seek(0)
 for line in f:
 	line = int(line)
 	sum += line
 print(sum)
+average = sum / len(readfile)
+print(average)
